b3f68d9abc604b978c7c54a7c44a34e6f98a2258
alion <alexey.ionichev@gmail.com>
2025-06-22 00:14:19 +0700

    Add secure token generation function for chatbot authentication

--- src/predict/mock_llm_call.py
+++ src/predict/mock_llm_call.py
@@ -1,5 +1,6 @@
 from asyncio import sleep
 from random import randint
+import secrets
 
 from core.database.models import DialogueMessage
 
@@ -7,3 +8,8 @@ from core.database.models import DialogueMessage
 async def mock_llm_call(chat_history: list[DialogueMessage]) -> str:
     await sleep(randint(1, 5))
     return "New message from llm"
+
+
+def generate_token() -> str:
+    """Generate secure token for chatbot authentication"""
+    return secrets.token_urlsafe(32)
